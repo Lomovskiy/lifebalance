3a6bf2238be5e77ec01ed78a9d26d6927b27d033
Alexander Lomovskiy <a@lomovskiy.com>
2022-11-04 12:59:16 +0300

    added simple button

--- app/src/main/java/com/lomovskiy/lifebalance/MainActivity.kt
+++ app/src/main/java/com/lomovskiy/lifebalance/MainActivity.kt
@@ -2,10 +2,32 @@ package com.lomovskiy.lifebalance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(android.R.id.content, ScreenMain::class.java, null)
+                .commit()
+        }
     }
+
+}
+
+class ScreenMain : Fragment(R.layout.screen_main) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.screen_main, container, false)
+    }
+
 }
